0e6f1d19de01eb2e3711b48f1c98117bd69448a7
Bart Massey <bart.massey@gmail.com>
2024-03-09 21:25:02 -0800

    finished initial scanout demo

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Bart Massey 2024
 
 This crate demos display scanout using Embassy on the
 MicroBit v2.
+
+The scanout happens at increasing frame rate, starting at 1
+fps. See the source for details. As scanout gets faster, the
+illusion of a solidly-lit display increases.
+
+Move your head or jiggle the board as the frame rate gets
+higher to see some effects.
+
+Note that a "real" scanout should happen five times faster,
+as a whole row can be illuminated at once. On the other
+hand, a "real" scanout will allow multiple perceived
+brightness levels: with 10 brightness levels, the scanout
+will take 10 times as long.
--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,27 @@ async fn main(_spawner: Spawner) -> ! {
         r.set_low();
     }
     for c in &mut cols {
-        c.set_low();
+        c.set_high();
     }
 
     loop {
-        for r in &mut rows {
-            r.set_high();
-            for c in &mut cols {
-                c.set_high();
-                Timer::after_millis(100).await;
-                c.set_low();
+        let frame_rates = (1..9).step_by(2).chain((10..60).step_by(5));
+
+        for fr in frame_rates {
+            let delay_time = 1_000_000 / (fr * 25);
+            for _ in 0..5 * fr {
+                for r in &mut rows {
+                    r.set_high();
+                    for c in &mut cols {
+                        c.set_low();
+                        Timer::after_micros(delay_time).await;
+                        c.set_high();
+                    }
+                    r.set_low();
+                }
             }
-            r.set_low();
+            Timer::after_millis(250).await;
         }
+        Timer::after_millis(2000).await;
     }
 }
